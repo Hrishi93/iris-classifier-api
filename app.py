690f361056135e406e6c6043101e9e9d83a2437a
Hrishikesh Mahure <136562585+Hrishi93@users.noreply.github.com>
2025-09-16 13:01:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Iris Classifier API is Running!"  # Simple message to confirm the server is active
+    return "Hi Hrishi, Iris Classifier API is Running!"  # Simple message to confirm the server is active
 
 @app.route("/predict", methods=["POST"])
 def predict():
